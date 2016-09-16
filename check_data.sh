16c84db58c0e22518362d3cdca63638aede9739f
Miroslav Jirik <mjirik@kky.zcu.cz>
2016-09-16 09:09:48 +0200

    rename file

--- check_data.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avv /data-kky/public/queetech/medical/processed/metalisa/ /auto/plzen1/home/mjirik/data/medical/processed/metalisa/
---
+++ get_data.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -avv /data-kky/public/queetech/medical/processed/metalisa/ /auto/plzen1/home/$USER/data/medical/processed/metalisa/

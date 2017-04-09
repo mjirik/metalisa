bc89d1e5488a817c088427dec425d4062d15028f
M. Jirik <miroslav.jirik@gmail.com>
2017-04-09 15:59:31 +0200

    data preparation

--- tests/metalisa_test.py
+++ tests/metalisa_test.py
@@ -9,8 +9,20 @@ class MyTestCase(unittest.TestCase):
         ed = sed3.sed3(data3d)
         ed.show()
 
-        self.assertEqual(True, False)
+        # self.assertEqual(True, False)
 
+    def test_train(self):
+        data_path = "sample_data"
+        data3d = metalisa.sample_data(data_path)
+        # ed = sed3.sed3(data3d)
+        # ed.show()
+
+        # train
+        metalisa.train(data_path)
+
+        df = metalisa.predict(data_path)
+        self.assertEquals(df[df["Slice_number"] == 1]["Numeric_Label"], 2)
+        # self.assertEquals(df[df["Slice_number"] == 1]["Text_Label"], "under_liver")
 
 if __name__ == '__main__':
     unittest.main()
